a38e8bd86abafedf532d11b47b55446b972104df
Rakesh <rakeshkraki3272@gmail.con>
2024-04-17 18:39:46 +0530

    assessments_17_04_2024

---
+++ ConsoleApp1/ConsoleApp1/WordCount.cs
@@ -0,0 +1,95 @@
+﻿//using System;
+
+class WordCount
+{
+    static void Main()
+
+    {
+
+        //CountWords();
+
+        //TimeZone();
+        
+        //CountWords();
+
+        //NumAvg();
+
+        //SimpleIntrest();
+
+        //MaxNumber();
+
+
+        //RemoveDup();
+
+        
+    }
+
+    
+
+    static void CountWords()
+    {
+        Console.WriteLine("Enter the String :- ");
+        string inputString = Console.ReadLine();
+        string[] words = inputString.Split(' ');
+        Console.WriteLine("Word Count :- " + words.Length);
+    }
+
+    static void TimeZone()
+    {
+        string myZone =Convert.ToString(TimeZoneInfo.Local);
+        Console.WriteLine("Local Timezone: " + myZone);
+
+    }
+
+    static void NumAvg()
+    {
+        int a =Convert.ToInt32(Console.ReadLine());
+        int b =Convert.ToInt32(Console.ReadLine());
+        int c =Convert.ToInt32(Console.ReadLine());
+        int avg = (a + b + c)/3;
+        Console.WriteLine("the avrage of numbers is :- " + avg);
+
+    }
+
+    static void SimpleIntrest()
+    {
+        int principaAmount = Convert.ToInt32(Console.ReadLine());
+        int rateOfIntrest = Convert.ToInt32(Console.ReadLine());
+        int TimeInYear = Convert.ToInt32(Console.ReadLine());
+
+        int si = (principaAmount *  rateOfIntrest * TimeInYear)/100;
+
+        Console.WriteLine(si);
+    }
+
+    static void MaxNumber()
+    {
+        int[] arr = { 1, 2, 3 };
+        int max = 0;
+
+        for(int i = 0;i < arr.Length; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+
+        Console.WriteLine("maximum number is :- " + max);
+    }
+
+    static void RemoveDup()
+    {
+        {
+            int[] array = { 1, 2, 2, 3, 4, 4, 5 };
+
+            int[] uniqueArray = array.Distinct().ToArray();
+
+            foreach (var item in uniqueArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
--- ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50b69b2468420829fbcd23922f6f7bb7d2450d9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64ab0c36d951c849084c86f75784782e9f971dbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
